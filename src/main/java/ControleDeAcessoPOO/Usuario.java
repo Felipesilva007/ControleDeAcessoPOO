b67998b527a02192c46e91326190cb5152414761
Aluno <leandrosinigaliaeloy2@gmail.com>
2025-01-21 16:07:13 -0300

    dd

--- src/main/java/ControleDeAcessoPOO/Usuario.java
+++ src/main/java/ControleDeAcessoPOO/Usuario.java
@@ -4,16 +4,19 @@ public class Usuario {
     String nome;
     String email;
     int telefone;
+    int Id;
+    int IdAcesso;
 
-
-    public Usuario(String nome, String email, int telefone) {
+    public Usuario(String nome, String email, int telefone, int ID, int IdAcesso) {
         this.nome = nome;
         this.email = email;
         this.telefone = telefone;
+        this.Id =ID;
+        this.IdAcesso=IdAcesso;
     }
 
     @Override
     public String toString() {
-        return String.format("%s | %s | %s", nome, email, telefone);
+        return String.format("%s | %s | %s | %s | %s", Id,IdAcesso,nome,telefone,email);
     }
 }
