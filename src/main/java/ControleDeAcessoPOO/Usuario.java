73a639bce2643571e0a245a90279f805238663b9
Aluno <leandrosinigaliaeloy2@gmail.com>
2025-01-21 15:35:58 -0300

    dd

--- src/main/java/ControleDeAcessoPOO/Usuario.java
+++ src/main/java/ControleDeAcessoPOO/Usuario.java
@@ -2,8 +2,18 @@ package ControleDeAcessoPOO;
 
 public class Usuario {
     String nome;
-    String emal;
+    String email;
     int telefone;
-    int qtdUsuario;
 
+
+    public Usuario(String nome, String email, int telefone) {
+        this.nome = nome;
+        this.email = email;
+        this.telefone = telefone;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("%s | %s | %s", nome, email, telefone);
+    }
 }
